8b504ab42e2d1db89806bdee4e29b41b7b9c90aa
silvsilvsilv <johnleonilsilva24@gmail.com>
2024-09-23 22:03:53 +0800

    copy pasted a Dashboard from shadcn

---
+++ app/homepage.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+
+export default function Homepage(){
+
+    return(
+        <>
+            <video src="C:\Users\Silva\Videos\Overwolf\Outplayed\Valorant\Valorant_08-24-2024_21-56-53-975\Valorant_08-24-2024_22-19-6-502.mp4"></video>
+            text
+            <Image src="C:\Users\Silva\Pictures\Screenshots\Screenshot (1).png"
+                alt="asd"
+                width={180}
+                height={38}
+                ></Image>
+                text
+        </>
+    )
+}
