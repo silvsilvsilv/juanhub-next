c4b0eeb1435485c9b54e386930866ddf9254e3f3
silvsilvsilv <johnleonilsilva24@gmail.com>
2024-09-25 12:51:25 +0800

    made weird colors disappear

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
         className={`antialiased`}
       >
         {children}
-        {/* <Homepage/> */}
+       
       </body>
     </html>
   );
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Dashboard(){
+
     return(
         <>
-            <h1> YOOOO</h1>
+           <Button> 
+                <Link href="/">BANGSAT</Link>
+           </Button>
         </>
     );
 }
