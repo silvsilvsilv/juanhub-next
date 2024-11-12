9785951cb98a29fa55a7bd9f2681a5af378bdcbf
silvsilvsilv <johnleonilsilva24@gmail.com>
2024-11-13 03:07:39 +0800

    fixing fonts

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "../globals.css";
+import { Poppins,Montserrat } from "next/font/google";
 
 const geistSans = localFont({
   src: "../fonts/GeistVF.woff",
@@ -12,6 +13,20 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+
+const poppins = Poppins({
+  weight:['400','700'],
+  subsets:['latin'],
+  display:'swap',
+});
+
+const montserrat = Montserrat({
+  weight:['400','700'],
+  subsets:['latin'],
+  display:'swap',
+});
+
+
 
 export const metadata: Metadata = {
   title: "Juan Hub",
@@ -27,7 +42,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={` ${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={` ${poppins.className} ${montserrat.className} ${geistMono.variable} ${geistSans.variable} antialiased`}
       >
         {children}
        
