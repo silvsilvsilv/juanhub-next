e6af6541b2a0c49f300ae7daf00eeae35208f9a6
silvsilvsilv <johnleonilsilva24@gmail.com>
2024-12-15 22:51:00 +0800

    can now add title to images

--- app/subscribe/page.tsx
+++ app/subscribe/page.tsx
@@ -5,12 +5,18 @@ import axios from 'axios';
 
 const UploadImage = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [imageTitle, setImageTitle] = useState<string>("");
 
   // Update the selected file state when the user selects a file
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0]; // Get the first selected file
     setSelectedFile(file || null);
   };
+
+  const handleText = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const title = e.target.value;
+    setImageTitle(title);
+  }
 
   // Upload the selected file
   const handleUpload = async () => {
@@ -20,23 +26,26 @@ const UploadImage = () => {
     }
 
     const userId = localStorage.getItem('userId'); // Get user ID from localStorage
+    
     if (!userId) {
       console.error('User ID not found in localStorage');
       return;
     }
 
     const formData = new FormData();
-    const title = "HELLOOOOOO";
+  
     formData.append('image', selectedFile); // Append the selected image
     formData.append('user_id', userId); // Append the user ID
-    formData.append('title',title);
+    formData.append('title', imageTitle);
 
     try {
       const response = await axios.post('http://localhost:8000/api/images', formData, {
         headers: { 'Content-Type': 'multipart/form-data' },
       });
+
       console.log('Image uploaded successfully:', response.data);
       alert('Image uploaded successfully!');
+
     } catch (error) {
       console.error('Error uploading image:', error.response?.data || error.message);
       alert('Image upload failed. Check the console for more details.');
@@ -49,6 +58,8 @@ const UploadImage = () => {
       <button onClick={handleUpload} disabled={!selectedFile}>
         Upload Image
       </button>
+      <br></br>
+      <input type="text" onChange={handleText} placeholder='Image Title'/>
     </div>
   );
 };
