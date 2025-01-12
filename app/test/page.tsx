41d5c1d894ff82a33f2a0d54c8f5dd9da5f0b82e
silvsilvsilv <johnleonilsilva24@gmail.com>
2025-01-12 21:28:11 +0800

    added testing route

---
+++ app/test/page.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { PhotoCard } from "@/components/photo-card";
+import axios from "axios";
+import { useEffect, useState } from "react"
+
+interface Image {
+  id: number;
+  user_id: number;
+  title: string;
+  url: string;
+  created_at:string;
+  onDelete: () => Promise<void>;
+  onAddToAlbum: () => void;
+}
+
+export default function Test() {
+  
+  const [images, setImages] = useState<Image[]>([]);
+  
+ useEffect(() => {
+    const fetchImages = async () => {
+       try {
+            const userId = localStorage.getItem('userId');
+            const response = await axios.get(`https://ivory-llama-451678.hostingersite.com/api/images`, {
+            params: {
+                user_id: userId,
+            },
+            });
+            setImages(response.data);
+            console.log(response.data);
+        } catch (error) {
+            console.error('Error fetching images:', error);
+        }
+    };
+
+    fetchImages();
+  }, []);
+
+  return(
+    <>
+      {images.map((photo, index) => (
+                  <PhotoCard key={index} {...photo} />
+                ))}
+    </>
+    
+  );
+
+}
