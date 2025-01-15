c5f756cde0ac0fa22dd36eedfff6a8f76bc1208f
silvsilvsilv <johnleonilsilva24@gmail.com>
2025-01-15 17:31:00 +0800

    fixing build error

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,43 +1,9 @@
 'use client'
 
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-
-interface Comment {
-  id: number
-  user_id: number
-  content: string
-  created_at: string
-  image_id:number
-  isEditing?: boolean
-  replies:[Comment]
-}
-
-export default function Test({id=2}) {
-
-  const [comments, setComments] = useState<Comment[]>([]);
-  useEffect(() => {
-    const fetchComments = async () =>{
-      try {
-        const imageId = id;
-        const response = await axios.get(`https://ivory-llama-451678.hostingersite.com/api/images/${imageId}/comments`);
-        setComments(response.data);
-        console.log(response.data);
-      } catch (error) {
-          console.error('Error fetching comments:', error);
-      }
-    }
-  
-    fetchComments();
-  }, [id])
-
+export default function Test(){
   return(
-    <>
-      <div>
-        {comments.map((comment) => (<p key={comment.id}>{comment.content}</p>) )}
-      </div>
-    </>
-    
-  );
-
+    <div>
+      Hello World
+    </div>
+  )
 }
